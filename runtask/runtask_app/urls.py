7f1249203772e9e957d94ff54af01a00c5f544e8
J3V2 <150044457+J3V2@users.noreply.github.com>
2024-12-16 14:10:33 +0800

    Update Leader Features and User Features

--- runtask/runtask_app/urls.py
+++ runtask/runtask_app/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
 from . import views
-from .views import login
+from .views import runtask
 
 urlpatterns = [
-    path('', views.login, name = 'login'),
-    path('register/', views.register, name = 'register'),
-    path('leader/view-task', views.leader_view_task, name = 'leader_view_task'),
-    path('leader/view-user', views.leader_view_user, name='leader_view_user')
+    path('', views.runtask, name = 'runtask'),
+
+    path('member/login/', views.member_login, name = 'member_login'),
+    path('member/register/', views.member_register, name='member_register'),
+    path('member/view-task/', views.member_view_task, name='member_view_task'),
+    path('member/view-user/', views.member_view_user, name='member_view_user'),
+    path('member/menu-label/', views.member_menu_label, name='member_menu_label'),
+    path('member/settings/', views.member_settings, name='member_settings'),
+
+    path('leader/login/', views.leader_login, name = 'leader_login'),
+    path('leader/register/', views.leader_register, name = 'leader_register'),
+    path('leader/view-task/', views.leader_view_task, name = 'leader_view_task'),
+    path('leader/view-user/', views.leader_view_user, name='leader_view_user'),
+    path('leader/menu-label/', views.leader_menu_label, name='leader_menu_label'),
+    path('leader/settings/', views.leader_settings, name='leader_settings'),
 ]
--- runtask/runtask_app/views.py
+++ runtask/runtask_app/views.py
@@ -2,14 +2,44 @@ from django.shortcuts import render, redirect
 from .forms import *
 
 # Create your views here.
-def login(request):
-    return render(request, 'login/login.html')
+def runtask(request):
+    return render(request, 'login/runtask.html')
 
-def register(request):
-    return render(request, 'login/register.html')
+# Member
+def member_login(request):
+    return render(request, 'login/member_login.html')
+
+def member_register(request):
+    return render(request, 'login/member_register.html')
+
+def member_view_task(request):
+    return render(request, 'member/view_task.html')
+
+def member_view_user(request):
+    return render(request, 'member/view_user.html')
+
+def member_menu_label(request):
+    return render(request, 'member/menu_label.html')
+
+def member_settings(request):
+    return render(request, 'member/settings.html')
+
+
+# Leader
+def leader_login(request):
+    return render(request, 'login/leader_login.html')
+
+def leader_register(request):
+    return render(request, 'login/leader_register.html')
 
 def leader_view_task(request):
     return render(request, 'leader/view_task.html')
 
 def leader_view_user(request):
     return render(request, 'leader/view_user.html')
+
+def leader_menu_label(request):
+    return render(request, 'leader/menu_label.html')
+
+def leader_settings(request):
+    return render(request, 'leader/settings.html')
